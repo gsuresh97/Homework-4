60696ecc2a2759c2fdce29c4d049861fb7d01ea4
Gopi Suresh <suresh.gopikrishnan@gmail.com>
2016-02-29 14:46:30 -0800

    Done

--- Homework 4/list.cpp
+++ Homework 4/list.cpp
@@ -1,8 +1,8 @@
-//void listAll(const Class* c, string path)  // two-parameter overload
-//{
-//    path += c->name();
-//    cout << path << endl;
-//    for (int i = 0; i < c->subclasses().size(); i++) {
-//        listAll(c->subclasses()[i]  , path + "=>");
-//    }
-//}
+void listAll(const Class* c, string path)  // two-parameter overload
+{
+    path += c->name();
+    cout << path << endl;
+    for (int i = 0; i < c->subclasses().size(); i++) {
+        listAll(c->subclasses()[i]  , path + "=>");
+    }
+}
